e6806b384cc68b61ad20fad75332e6be46cb6dfc
fgd1987 <fgd1987@yeah.net>
2016-03-21 17:38:53 +0800

    init

---
+++ Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationManager : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+        bool ok = NetManager.Instance.Connect("192.168.1.222", 8888);
+        if (!ok)
+        {
+            Debug.Log("连接服务器失败！");
+            return;
+        }
+		Debug.Log("连接服务器成功！");
+
+        NetManager.Instance.StartNetThread();
+
+        LoginController.Instance().RegisterProto();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MessageDispatcher.Instance.DispatchMessage();
+	}
+
+    void OnApplicationQuit()
+    {
+        NetManager.Instance.Disconnect();
+    }
+}
